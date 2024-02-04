ea641ebd2f00ddb7e7fa16b06260259097f5a303
LokeshSaipureddi <aipureddilokesh@gmail.com>
2024-02-04 16:53:58 -0500

    commit

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 from src.CNNClassifier import logger
-logger.info(" This is a cutsom log ")
+from src.CNNClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
